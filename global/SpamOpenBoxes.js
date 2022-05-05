171fdb74066e1c6bb30fb88f7e25881d378a1bee
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-05 16:45:14 -0500

    Update SpamOpenBoxes.js

--- global/SpamOpenBoxes.js
+++ global/SpamOpenBoxes.js
@@ -7,23 +7,7 @@ function buyBox() {
     var postData = 'crate=' + name;
     $.post('/api/open/', postData, function(data) {
         blookRarity = window.elementList[data].rarity
-        if (blookRarity === "Uncommon") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lime;', `${blookUnlocked}`);
-        } else if (blookRarity === "Rare") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px blue;', `${blookUnlocked}`);
-        } else if (blookRarity === "Epic") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px red;', `${blookUnlocked}`);
-        } else if (blookRarity === "Legendary") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px gold;', `${blookUnlocked}`);
-        } else if (blookRarity === "Chroma") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px skyblue;', `${blookUnlocked}`);
-        } else if (blookRarity === "Perfect") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lemonchiffon;', `${blookUnlocked}`);
-        } else if (blookRarity === "Mystical") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px plum;', `${blookUnlocked}`);
-        } else if (blookRarity === "Divine") {
-            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px violet;', `${blookUnlocked}`);
-        } else if (blookRarity === undefined) {}
+        console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px black;', `${window.elementList[data]});
         if (data === "You're being rate limited.") {
             i--;
         }
