0119c03f21072c1a2770093cea2c10328516f799
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-06 05:34:08 -0500

    Create autoDeclineTrade.js

---
+++ global/autoDeclineTrade.js
@@ -0,0 +1,20 @@
+class Audio {
+	constructor(e) {
+		if (e === '/audio/tradeRequest.mp3') {
+			this.play = () => {
+				return 0;
+			}
+			return;
+		};
+		this.r = document.createElement('audio')
+		this.r.setAttribute('preload', 'auto')
+		this.r.setAttribute('src', e)
+		document.head.append(this.r)
+		this.play = async () => {
+			await this.r.play()
+			setTimeout(() => {
+				this.r.remove()
+			}, this.r.duration * 1000)
+		}
+	}
+}
