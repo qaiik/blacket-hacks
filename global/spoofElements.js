96df9f010188acd9b6778cf9558c5a7803e77bdf
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-06 14:14:05 -0500

    Create spoofElements.js

---
+++ global/spoofElements.js
@@ -0,0 +1,8 @@
+Object.entries(elementList).forEach((entry) => {
+	const [key, value] = entry;
+	$(`<img id="${key}" src="${elementList[key].imageURL}" onclick="viewElement('${key}')" class="bottomElement">`).appendTo(".elementList");
+})
+for (i=0;i<Object.keys(elementList).length;i++) {
+    elemes = Object.keys(elementList)
+    userElements[elemes[i]] = Math.floor(Math.random() * 100);
+}
