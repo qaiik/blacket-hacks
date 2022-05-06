d5efc70f811a594e4480d118d0bcb6408f4a7c24
zastix <69748767+notzastix@users.noreply.github.com>
2022-05-06 13:49:57 -0500

    box check

--- global/spamOpenCrates.js
+++ global/spamOpenCrates.js
@@ -1,15 +1,21 @@
-let name = prompt("Which crate would you like to open?\n\Options:\nall\nadd");
-let amt;
 let i = 1;
-amt = Number(prompt("How many crates would you like to open?"));
+let boxes = []
+Object.keys(cratesList).forEach(e => {
+    boxes.push(e)
+})
+boxes2 = boxes.join("\n")
+let name = prompt("Which crate would you like to open?\n\nOptions:\n" + boxes2);
+if (!boxes.includes(name)) {
+    alert('That crate doestn exist..')
+    name = prompt("Which crate would you like to open?\n\nOptions:\n" + boxes2);
+}
+let amt = Number(prompt("How many crates would you like to open?"));
 
 function buyBox() {
     var postData = 'crate=' + name;
     $.post('/api/open/', postData, function(data) {
-        console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px black;', `${data}`);
-        if (data === "You're being rate limited.") {
-            i--;
-        }
+        if (data === "You're being rate limited.") i--
+        else console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px black;', `${data}`);
     });
 }
 var check = setInterval(() => {
